c04b05f0eccfe80b06693a030a45383e1cfd4c9d
maxim-ctr <markisss75@gmail.com>
2021-09-28 12:34:06 +0300

    1 step

---
+++ Andriyanov_lab/Andriyanov_lab/Andriyanov_lab.cpp
@@ -0,0 +1,31 @@
+﻿// Andriyanov_lab.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+using namespace std;
+struct Pipe
+{
+	int id;
+	int d;
+};
+
+void PrintPipe(Pipe& p)
+{
+  cout << "Truba " << p.id << " - diametr = " << p.d;
+}
+
+Pipe AddPipe()
+{
+	Pipe p;
+	p.id = 0;
+	cout << "Vvedite diametr\n";
+	cin >> p.d;
+	return p;
+}
+
+int main()
+{
+	Pipe p = AddPipe();
+	PrintPipe(p);
+}
+
