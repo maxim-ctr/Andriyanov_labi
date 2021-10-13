73614525baa34018b2b161aa13bf67befeeecc29
maxim-ctr <markisss75@gmail.com>
2021-10-13 21:23:53 +0300

    7 step

--- Andriyanov_lab/Andriyanov_lab/Andriyanov_lab.cpp
+++ Andriyanov_lab/Andriyanov_lab/Andriyanov_lab.cpp
@@ -92,6 +92,90 @@ void PipeEdit(Pipe& p) {
 	}
 }
 
+struct Station {
+	int id = 0;
+	string name;
+	int number_of_workshops;
+	int number_of_working_workshops;
+	int Efficiency;
+};
+
+bool StExists(const Station& s)
+{
+	return s.id != -1;;
+}
+
+Station AddStation(int id) {
+	cout << "Adding Station\n";
+	Station s;
+
+	cout << "Input name:\n";
+	cin >> s.name;
+
+	cout << "Input number of workshops:\n";
+	s.number_of_workshops = 0;
+	while (s.number_of_workshops <= 0) {
+		s.number_of_workshops = input_integer();
+	}
+
+	cout << "Input number of working workshops:\n";
+	s.number_of_working_workshops = -1;
+	while (s.number_of_working_workshops < 0 || s.number_of_working_workshops > s.number_of_workshops) {
+		s.number_of_working_workshops = input_integer();
+		if (s.number_of_working_workshops > s.number_of_workshops) {
+			cout << "Wrong input\n";
+			cout << "Try again:\n";
+		}
+	}
+
+	cout << "Input Efficiency (0 < e <= 100):\n";
+	s.Efficiency = 0;
+	while (s.Efficiency <= 0 || s.Efficiency > 100) {
+		s.Efficiency = input_integer();
+	}
+	return s;
+}
+
+void StationEdit(Station& s) {
+	// if (array_of_stations != 0){}
+	if (StExists(s)) {
+		cout << "Editing station\n";
+		cout << "Input number of working workshops:\n";
+		s.number_of_working_workshops = -1;
+		while (s.number_of_working_workshops < 0 || s.number_of_working_workshops > s.number_of_workshops) {
+			s.number_of_working_workshops = input_integer();
+			if (s.number_of_working_workshops > s.number_of_workshops) {
+				cout << "Wrong input\n";
+				cout << "Try again:\n";
+			}
+		}
+	}
+	else {
+		cout << "\nNo stations\n";
+	}
+}
+
+void Output(const Pipe& p, const Station& s)
+{
+	cout << "\nOutput\n";
+	// if (vector_of_Pipes.length != 0
+	if (p.length > 0) {
+		cout << "\nOutput Pipe(s)";
+		cout << "\nId: " << p.id;
+		cout << "\nDiameter: " << p.diameter;
+		cout << "\nLength: " << p.length << "\n";
+	}
+	// if (vector_of_Station.length != 0){}
+	if (s.name != "") {
+		cout << "\nOutput station(s)";
+		cout << "\nId: " << s.id;
+		cout << "\nname: " << s.name;
+		cout << "\nnumber of workshops: " << s.number_of_workshops;
+		cout << "\nnumber of working workshops: " << s.number_of_working_workshops;
+		cout << "\nEfficiency: " << s.Efficiency << "\n";
+	}
+}
+
 
 
 int main()
